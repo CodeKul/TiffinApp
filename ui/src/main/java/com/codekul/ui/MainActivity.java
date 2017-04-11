1f71559ac080a8e780e5f3439c69b691ab88a321
aniruddha <aniruddha.kudalkar@gmail.com>
2017-04-11 20:24:19 +0530

    ui: making ui through code

--- ui/src/main/java/com/codekul/ui/MainActivity.java
+++ ui/src/main/java/com/codekul/ui/MainActivity.java
@@ -1,13 +1,29 @@
 package com.codekul.ui;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        final LinearLayout rootLayout = new LinearLayout(this);
+        rootLayout.setOrientation(LinearLayout.VERTICAL);
+        rootLayout.setLayoutParams(new ViewGroup.LayoutParams(300,300));
+
+        final EditText edtName = new EditText(this);
+        edtName.setHint("User Name");
+        edtName.setLayoutParams(new LinearLayout.LayoutParams(300,80));
+        rootLayout.addView(edtName);
+
+        setContentView(rootLayout);
     }
 }
