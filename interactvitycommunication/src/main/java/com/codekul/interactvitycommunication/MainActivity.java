dea3170f4ba1058f37dd3d0643a1034168a693f7
aniruddha <aniruddha.kudalkar@gmail.com>
2017-04-13 20:55:44 +0530

    inter activity communication : passing data to new activity and coming to earlier activity

--- interactvitycommunication/src/main/java/com/codekul/interactvitycommunication/MainActivity.java
+++ interactvitycommunication/src/main/java/com/codekul/interactvitycommunication/MainActivity.java
@@ -1,11 +1,17 @@
 package com.codekul.interactvitycommunication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final int REQ_NXT = 7562;
+    public static final String KEY_MY_NM = "myName";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +20,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void next(View view) {
+
+        Context context = this;
         Class cls = NextActivity.class;
 
-        Intent intent = new Intent(this, cls);
-        startActivity(intent);
+        EditText edtName = (EditText) findViewById(R.id.edtName);
+        String name = edtName.getText().toString();
+
+        Bundle bundle = new Bundle();
+        bundle.putString(KEY_MY_NM, name);
+
+        Intent intent = new Intent(context, cls);
+        intent.putExtras(bundle);
+
+        //startActivity(intent);
+
+        startActivityForResult(intent, REQ_NXT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_NXT) {
+            if (resultCode == RESULT_OK) {
+
+                Bundle bundle = data.getExtras();
+                String dataRes = bundle.getString(NextActivity.KEY_DATA_RES);
+
+                EditText edtName = (EditText) findViewById(R.id.edtName);
+                edtName.setText(dataRes);
+            }
+        }
     }
 }
--- interactvitycommunication/src/main/java/com/codekul/interactvitycommunication/NextActivity.java
+++ interactvitycommunication/src/main/java/com/codekul/interactvitycommunication/NextActivity.java
@@ -1,13 +1,60 @@
 package com.codekul.interactvitycommunication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class NextActivity extends AppCompatActivity {
 
+    public static final String KEY_DATA_RES = "dataRes";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        Intent resInt = getIntent();
+
+        Bundle bundle = resInt.getExtras();
+        String name = bundle.getString(MainActivity.KEY_MY_NM);
+
+        if(name != null && name.length() > 0) {
+            /*TextView textView = (TextView) findViewById(R.id.textInfo);
+            textView.setText(name);*/
+            text(name);
+        }
+    }
+
+    public void back(View view) {
+
+       /* TextView textView = (TextView) findViewById(R.id.textInfo);
+        String nameUpper = textView.getText().toString();*/
+
+        Bundle bundle = new Bundle();
+        //bundle.putString(KEY_DATA_RES, ((TextView) findViewById(R.id.textInfo)).getText().toString());
+        bundle.putString(KEY_DATA_RES, text());
+
+        Intent intent = new Intent();
+        intent.putExtras(bundle);
+
+        setResult(RESULT_OK, intent);
+
+        finish();
+    }
+
+    public void upper(View view) {
+        /*TextView textView = (TextView) findViewById(R.id.textInfo);
+        String nameUpper = textView.getText().toString().toUpperCase();
+        textView.setText(nameUpper);*/
+        text(text());
+    }
+
+    private String text() {
+        return ((TextView) findViewById(R.id.textInfo)).getText().toString();
+    }
+    private void text(String nm) {
+        TextView textView = (TextView) findViewById(R.id.textInfo);
+        textView.setText(nm);
     }
 }
