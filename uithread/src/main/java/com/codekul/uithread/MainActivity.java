865dae0128f90ff9ef51b95b0ea1f323cd5febc5
aniruddha <aniruddha.kudalkar@gmail.com>
2017-04-25 20:54:48 +0530

    uithread : async task and handlers

---
+++ uithread/src/main/java/com/codekul/uithread/MainActivity.java
@@ -0,0 +1,107 @@
+package com.codekul.uithread;
+
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handler = new Handler(Looper.getMainLooper());
+    }
+
+    public void counter(View view) {
+//        new MyTask().execute(1,100);
+
+        handled();
+    }
+
+    /* TODO: call this method in counter */
+    private void threaded() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <100 ;i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+//                    ((TextView)findViewById(R.id.textCounter)).setText(String.valueOf(i));
+                }
+            }
+        }).start();
+    }
+
+    private class MyTask extends AsyncTask<Integer/*Params*/, Integer/*progress*/, Boolean/*Result*/>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            // Ui Thread
+        }
+
+        @Override
+        protected Boolean/*Result*/ doInBackground(Integer... params/*params*/) {
+            // Worker Thread
+
+            for (int i = params[0]; i< params[1]; i++){
+                publishProgress(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean/*Result*/) {
+            super.onPostExecute(aBoolean);
+            // Ui Thread
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values/*progress , int array*/) {
+            super.onProgressUpdate(values);
+
+            //Ui thread
+            ((TextView)findViewById(R.id.textCounter)).setText(String.valueOf(values[0]));
+        }
+    }
+
+    int i = 0;
+
+    private void handled() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for ( i = 0; i <100 ;i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((TextView)findViewById(R.id.textCounter)).setText(String.valueOf(i));
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+}
