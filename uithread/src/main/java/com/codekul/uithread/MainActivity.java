7e742448d529ec385975f32434aae62b1d1eb54c
aniruddha <aniruddha.kudalkar@gmail.com>
2017-04-25 21:19:06 +0530

    uithread : rxjava implementation

--- uithread/src/main/java/com/codekul/uithread/MainActivity.java
+++ uithread/src/main/java/com/codekul/uithread/MainActivity.java
@@ -5,24 +5,39 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         handler = new Handler(Looper.getMainLooper());
+
     }
 
     public void counter(View view) {
 //        new MyTask().execute(1,100);
 
-        handled();
+        //handled();
+
+
+        counterObservable()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.newThread())
+                .doOnNext( i -> ((TextView)findViewById(R.id.textCounter)).setText(String.valueOf(i)))
+                .doOnComplete( () -> Log.i("@codekul","Completed"))
+                .subscribe();
     }
 
     /* TODO: call this method in counter */
@@ -30,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i <100 ;i++) {
+                for (int i = 0; i < 100; i++) {
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -42,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
-    private class MyTask extends AsyncTask<Integer/*Params*/, Integer/*progress*/, Boolean/*Result*/>{
+    private class MyTask extends AsyncTask<Integer/*Params*/, Integer/*progress*/, Boolean/*Result*/> {
 
         @Override
         protected void onPreExecute() {
@@ -55,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         protected Boolean/*Result*/ doInBackground(Integer... params/*params*/) {
             // Worker Thread
 
-            for (int i = params[0]; i< params[1]; i++){
+            for (int i = params[0]; i < params[1]; i++) {
                 publishProgress(i);
                 try {
                     Thread.sleep(1000);
@@ -77,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
             super.onProgressUpdate(values);
 
             //Ui thread
-            ((TextView)findViewById(R.id.textCounter)).setText(String.valueOf(values[0]));
+            ((TextView) findViewById(R.id.textCounter)).setText(String.valueOf(values[0]));
         }
     }
 
@@ -87,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for ( i = 0; i <100 ;i++) {
+                for (i = 0; i < 100; i++) {
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -97,11 +112,25 @@ public class MainActivity extends AppCompatActivity {
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
-                            ((TextView)findViewById(R.id.textCounter)).setText(String.valueOf(i));
+                            ((TextView) findViewById(R.id.textCounter)).setText(String.valueOf(i));
                         }
                     });
                 }
             }
         }).start();
+    }
+
+    private Observable<Integer> counterObservable() {
+        return Observable.create(emitter -> {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                emitter.onNext(i);
+            }
+            emitter.onComplete();
+        });
     }
 }
