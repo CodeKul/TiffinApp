30b3457e7638a8332e3c4e1d2b0dfce6d3080b14
aniruddha <aniruddha.kudalkar@gmail.com>
2017-05-31 20:57:00 +0530

    camera

---
+++ camera/src/main/java/com/codekul/camera/MainActivity.java
@@ -0,0 +1,94 @@
+package com.codekul.camera;
+
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import io.reactivex.functions.Consumer;
+import me.shaohui.advancedluban.Luban;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int REQ_CAPTURE = 1234;
+    private Uri imageUri;
+    private File captured;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void capture(View view) {
+
+        captured = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), System.currentTimeMillis() + ".jpg");
+        imageUri = Uri.fromFile(captured);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(captured));
+
+        startActivityForResult(intent, REQ_CAPTURE);
+    }
+
+    public void record(View view) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+
+              /*
+               // without compression
+               try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+
+              compress(captured);
+            }
+        }
+    }
+
+    private void compress(File file) {
+        Luban.compress(this, file)
+                .putGear(Luban.FIRST_GEAR)
+                .asObservable()
+                .subscribe(new Consumer<File>() {
+                    @Override
+                    public void accept(File file) throws Exception {
+
+                        byte []imgByts = new byte[(int) file.length()];
+                        FileInputStream fis = new FileInputStream(file);
+                        fis.read(imgByts);
+                        fis.close();
+
+                        FileOutputStream fos = new FileOutputStream(captured);
+                        fos.write(imgByts);
+                        fos.close();
+
+                        ((ImageView)findViewById(R.id.imageView)).setImageURI(Uri.fromFile(file));
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        throwable.printStackTrace();
+                    }
+                });
+    }
+}
